1caeadde19eb3a6083dd374b24f5b8871626cfd7
minh nguyen <nguyendangminh236@gmail.com>
2019-10-23 19:03:34 +0700

    OOP

--- OOP/Customer.php
+++ OOP/Customer.php
@@ -1,7 +1,15 @@
 <?php
 include 'User.php';
-class Customer extends User{
-
+class Customer extends User {
+    public $idCustomer;
+    public $sendAddress;
+    public function Pay() {
+        echo "Pay";
+    }
+    public function History() {
+        echo "History";
+    }
 }
-
+$customer = new Customer;
+$customer->Pay();
 ?>
